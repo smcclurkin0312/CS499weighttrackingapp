0072331f95410b8f0055505ef13945e42e5f9fb6
Stephanie McClurkin <stephanie.mcclurkin@snhu.edu>
2025-02-01 09:16:51 -0600

    Initial commit - Weight Tracking App

---
+++ app/src/main/java/com/example/project3_wta_sm/DataGridItem.kt
@@ -0,0 +1,10 @@
+package com.example.project3_wta_sm
+
+// Single weight entry
+data class DataGridItem(
+    // Date of weight entry
+    val date: String,
+
+    // Weight value of entry
+    val weight: Double
+)
